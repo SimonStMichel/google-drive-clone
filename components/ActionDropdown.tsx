c3911fe9a0bde256ebe8c818d25b9b17759ecb72
Simon St-Michel <simonstmichel@outlook.com>
2026-02-04 18:47:06 -0500

    share and delete file features

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -3,8 +3,15 @@
 import React, { useState } from "react";
 
 import { actionsDropdownItems } from "@/constants";
+import { constructDownloadUrl } from "@/lib/utils";
+import { deleteFile, renameFile, updateFileUsers } from "@/lib/actions/file.actions";
 
+import { Models } from "node-appwrite";
+
+import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+
 import { Button } from "./ui/button";
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import {
@@ -15,13 +22,10 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Models } from "node-appwrite";
-import Link from "next/link";
-import { constructDownloadUrl } from "@/lib/utils";
+
+
 import { Input } from "./ui/input";
-import { renameFile } from "@/lib/actions/file.actions";
-import { usePathname } from "next/navigation";
-import { FileDetails } from "./ActionsModalContent";
+import { FileDetails, ShareInput } from "./ActionsModalContent";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
     const path = usePathname();
@@ -31,6 +35,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
     const [action, setAction] = useState<ActionType | null>(null);
     const [name, setName] = useState(file.name);
     const [isLoading, setIsLoading] = useState(false);
+    const [emails, setEmails] = useState<string[]>([]);
 
     const closeAllModals = () => {
         setIsModalOpen(false);
@@ -49,8 +54,8 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
 
         const actions = {
             rename: () => renameFile({ fileId: file.$id, name, extension: file.extension, path }),
-            share: () => console.log("share"),
-            delete: () => console.log("delete"),
+            share: () => updateFileUsers({ fileId: file.$id, emails, path }),
+            delete: () => deleteFile({ fileId: file.$id, bucketFileId: file.bucketFileId, path }),
         };
 
         success = await actions[action.value as keyof typeof actions]();
@@ -60,6 +65,19 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
             setIsLoading(false);
         }
 
+    };
+
+    const handleRemoveUser = async (email: string) => {
+        const updatedEmails = emails.filter((e) => e !== email);
+
+        const success = await updateFileUsers({
+            fileId: file.$id,
+            emails: updatedEmails,
+            path,
+        });
+
+        if (success) setEmails(updatedEmails);
+        closeAllModals();
     };
 
     const renderDialogContent = () => {
@@ -72,7 +90,11 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
                 <DialogHeader className="flex flex-col gap-3">
                     <DialogTitle className="text-center text-light-100">{label}</DialogTitle>
                     {value === "rename" && <Input type="text" value={name} onChange={(e) => setName(e.target.value)} />}
+                    {value === "share" && <ShareInput file={file} onInputChange={setEmails} onRemove={handleRemoveUser} />}
                     {value === "details" && <FileDetails file={file} />}
+                    {value === "delete" && (
+                        <p className="delete-confirmation">Are you sure you want to delete {" "} <span className="delete-file-name">{file.name}</span>?</p>
+                    )}
                 </DialogHeader>
                 {["rename", "delete", "share"].includes(value) && (
                     <DialogFooter className="flex flex-col gap-3 md:flex-row">
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -136,3 +136,54 @@ export const renameFile = async ({ fileId, name, extension, path }: RenameFilePr
         handleError(error, "Failed to rename file");
     }
 };
+
+/**
+ * 
+ * @param param0 
+ * @returns 
+ */
+export const updateFileUsers = async ({ fileId, emails, path }: UpdateFileUsersProps) => {
+    const { databases } = await createAdminClient();
+
+    try {
+        const updatedFile = await databases.updateDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            fileId,
+            {
+                users: emails,
+            },
+        );
+
+        revalidatePath(path);
+        return parseStringify(updatedFile);
+    } catch (error) {
+        handleError(error, "Failed to update users list");
+    }
+};
+
+/**
+ * 
+ * @param param0 
+ * @returns 
+ */
+export const deleteFile = async ({ fileId, bucketFileId, path }: DeleteFileProps) => {
+    const { databases, storage } = await createAdminClient();
+
+    try {
+        const deletedFile = await databases.updateDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            fileId,
+        );
+
+        if (deletedFile) {
+            await storage.deleteFile(appwriteConfig.bucketId, bucketFileId);
+        }
+
+        revalidatePath(path);
+        return parseStringify({ status: "success" });
+    } catch (error) {
+        handleError(error, "Failed to update users list");
+    }
+};
